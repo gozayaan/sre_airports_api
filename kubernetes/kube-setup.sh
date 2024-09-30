6dd224a668645ce0b80a5f2b9a2df0a7b1065535
bijoy26 <42487891+bijoy26@users.noreply.github.com>
2024-10-01 04:11:41 +0600

    ADD Kubernetes manifests

---
+++ kubernetes/kube-setup.sh
@@ -0,0 +1,23 @@
+kubectl create ns go
+
+# GCS Mock Bucket
+kubectl create secret generic gcs-bucket-name-secret --from-literal=gcs-bucket-name="dev_bd_airport" -n go
+###### Kong API Gateway Setup ######
+
+# Install k8s Gateway API CRD
+kubectl apply -f https://github.com/kubernetes-sigs/gateway-api/releases/download/v0.7.1/standard-install.yaml
+
+# install kong gateway class & gateway
+kubectl apply -f ./network-setup/kong-apigw/gateway.yaml
+
+# install kong helm chart
+helm repo add kong https://charts.konghq.com
+helm repo update
+helm install kong/kong --generate-name --set ingressController.installCRDs=false -n kong --create-namespace
+
+# Enable gateway support in kong
+kubectl set env -n kong deployment/ingress-kong CONTROLLER_FEATURE_GATES="GatewayAlpha=true" -c ingress-controller
+kubectl rollout restart -n NAMESPACE deployment DEPLOYMENT_NAME
+
+# Deploy HTTPRoute attached to the Kong APIGW for traffic split routing
+kubectl apply -f ./network-setup/kong-apigw/httproute.yaml
